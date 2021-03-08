884e54596b42af0b12ae7057b528f8601809549c
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-03-08 16:18:00 -0600

    updated index.js

--- db/database.js
+++ db/database.js
@@ -9,6 +9,24 @@ const connection = mysql.createConnection({
   database: "employees"
 });
 
+// simple query
+connection.query(
+    'SELECT * FROM `table` WHERE `name` = "Page" AND `age` > 45',
+    function(err, results, fields) {
+      console.log(results); // results contains rows returned by server
+      console.log(fields); // fields contains extra meta data about results, if available
+    }
+);
+   
+// with placeholder
+connection.query(
+    'SELECT * FROM `table` WHERE `name` = ? AND `age` > ?',
+    ['Page', 45],
+    function(err, results) {
+      console.log(results);
+    }
+);
+
 // error function
 connection.connect(function (err) {
   if (err) throw err;
--- db/schema.sql
+++ db/schema.sql
@@ -1,6 +1,5 @@
 DROP DATABASE IF EXISTS employees;
 CREATE DATABASE employees;
-
 USE employees;
 
 DROP TABLE IF EXISTS department;
