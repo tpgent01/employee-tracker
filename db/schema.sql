639d40b6d2c29fdb7c9b06bfdac20e593bc4f426
tpgent01 <73126336+tpgent01@users.noreply.github.com>
2021-05-12 20:11:53 -0500

    getting error

--- db/schema.sql
+++ db/schema.sql
@@ -38,3 +38,4 @@ CREATE TABLE employee(
     PRIMARY KEY (id),
     CONSTRAINT fk_role FOREIGN KEY (role_id) REFERENCES role(id) ON DELETE CASCADE,
     CONSTRAINT fk_employee FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
